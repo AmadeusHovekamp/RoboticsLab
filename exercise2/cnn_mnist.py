6f19eb8d4a1ff9ac66024689865000f615ff5b0b
ah663 <amadeus.hovekamp@rwth-aachen.de>
2018-11-13 16:30:50 +0100

    running cnn network, random search and notebook to run and plot figures

--- exercise2/cnn_mnist.py
+++ exercise2/cnn_mnist.py
@@ -5,12 +5,12 @@ import gzip
 import json
 import os
 import pickle
+import shutil
 
 import numpy as np
 import tensorflow as tf
 
-tf.logging.set_verbosity(tf.logging.DEBUG)
-
+# tf.logging.set_verbosity(tf.logging.DEBUG)
 
 def one_hot(labels):
     """this creates a one hot encoding from a flat vector:
@@ -48,6 +48,16 @@ def mnist(datasets_dir='./data'):
         train_set, valid_set, test_set = pickle.load(f)
     f.close()
 
+    #print(test_set[0].shape, test_set[1].shape)
+    #print(valid_set[0].shape, valid_set[1].shape)
+    #print(train_set[0].shape, train_set[1].shape)
+    #test_set = (test_set[0][:300], test_set[1][:300])
+    #valid_set = (valid_set[0][:300], valid_set[1][:300])
+    #train_set = (train_set[0][:300], train_set[1][:300])
+    #print(test_set[0].shape, test_set[1].shape)
+    #print(valid_set[0].shape, valid_set[1].shape)
+    #print(train_set[0].shape, train_set[1].shape)
+
     test_x, test_y = test_set
     test_x = test_x.astype('float32')
     test_x = test_x.astype('float32').reshape(test_x.shape[0], 28, 28, 1)
@@ -60,126 +70,132 @@ def mnist(datasets_dir='./data'):
     train_x = train_x.astype('float32').reshape(train_x.shape[0], 28, 28, 1)
     train_y = train_y.astype('int32')
     print('... done loading data')
-    #return train_x, one_hot(train_y), valid_x, one_hot(valid_y), test_x, one_hot(test_y)
+    
+    # return train_x, one_hot(train_y), valid_x, one_hot(valid_y), test_x, one_hot(test_y)
     return train_x, train_y, valid_x, valid_y, test_x, test_y
 
-def cnn_model_fn(features, labels, mode):
-    print("\n\n\t\t\tcnn_model_fn called with mode:\t", mode, "\n\n")
 
-    """Model function for CNN."""
-    # Input Layer
-    input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
+def create_cnn_model(features, labels, mode):
+    # Construct the convolutional neural network (CNN).
 
-    # Convolutional Layer #1
-    conv1 = tf.layers.conv2d(
-        inputs=input_layer,
-        filters=16,
-        kernel_size=[3, 3],
-        padding="same",
-        activation=tf.nn.relu)
+    # Input layer.
+    layer_inputs = tf.reshape(features["x"], [-1, 28, 28, 1])
+    
+    # First convolutional layer.
+    layer_conv_1 = tf.layers.conv2d(inputs=layer_inputs,
+                                    filters=num_filters,
+                                    kernel_size=filter_size,
+                                    strides=1,
+                                    padding='same',
+                                    activation=tf.nn.relu)
 
-    # Pooling Layer #1
-    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=1)
+    # First pooling layer.
+    layer_pool_1 = tf.layers.max_pooling2d(inputs=layer_conv_1,
+                                           pool_size=2,
+                                           strides=1)
 
-    # Convolutional Layer #2 and Pooling Layer #2
-    conv2 = tf.layers.conv2d(
-        inputs=pool1,
-        filters=16,
-        kernel_size=[3, 3],
-        padding="same",
-        activation=tf.nn.relu)
-    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=1)
+    # Second convolutional layer.
+    layer_conv_2 = tf.layers.conv2d(inputs=layer_pool_1,
+                                    filters=num_filters,
+                                    kernel_size=filter_size,
+                                    strides=1,
+                                    padding='same',
+                                    activation=tf.nn.relu)
 
-    # Flatten to input to dense layer afterwards
-    pool2_flat = tf.contrib.layers.flatten(pool2)
+    # Second pooling layer.
+    layer_pool_2 = tf.layers.max_pooling2d(inputs=layer_conv_2,
+                                           pool_size=2,
+                                           strides=1)
 
-    # Dense Layer
-    dense = tf.layers.dense(inputs=pool2_flat, units=128, activation=tf.nn.relu)
+    # Fully connected layer.
+    layer_pool_2_flat = tf.contrib.layers.flatten(layer_pool_2)
 
-    # Logits Layer
-    logits = tf.layers.dense(inputs=dense, units=10)
+    layer_fully_connected = tf.layers.dense(inputs=layer_pool_2_flat, units=128, activation=tf.nn.relu)
 
+    # Output layer.
+    layer_output_wo_softmax = tf.layers.dense(inputs=layer_fully_connected, units=10)
 
     predictions = {
-        # Generate predictions (for PREDICT and EVAL mode)
-        "classes": tf.argmax(input=logits, axis=1), # axis=1
-        # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
-        # `logging_hook`.
-        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+        "classes": tf.argmax(input=layer_output_wo_softmax, axis=1),
+        "probabilities": tf.nn.softmax(layer_output_wo_softmax, name="softmax_tensor")
     }
-
 
     if mode == tf.estimator.ModeKeys.PREDICT:
         return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
-    # Calculate Loss (for both TRAIN and EVAL modes)
+    # Calculate loss
+    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=layer_output_wo_softmax)    
 
-    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-
-    # Configure the Training Op (for TRAIN mode)
+    # Configure training.
     if mode == tf.estimator.ModeKeys.TRAIN:
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001) ## TODO FIX ME
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=lr)
         train_op = optimizer.minimize(
             loss=loss,
             global_step=tf.train.get_global_step())
         return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
 
-    # Add evaluation metrics (for EVAL mode)
-    eval_metric_ops = {
-        "accuracy": tf.metrics.accuracy(
-            labels=labels, predictions=predictions["classes"])}
-    print("\n\n\t\t\t after eval_metric_ops \n\n")
-    return tf.estimator.EstimatorSpec(
-        mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+    # Evaluation metrics
+    eval_metric_ops = {"accuracy": tf.metrics.accuracy(labels=labels, predictions=predictions["classes"])}
+    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+    
 
 
 def train_and_validate(x_train, y_train, x_valid, y_valid, num_epochs, lr, num_filters, batch_size, filter_size):
-    # TODO: train and validate your convolutional neural networks with the provided data and hyperparameters
-    print("\n\n \t \t \t train_and_validate called \n \n \n ")
-    # Set up logging for predictions
-    tensors_to_log = {"probabilities": "softmax_tensor"}
-    logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log, every_n_iter=100) # TODO
+
+    # Empty working directory
+    model_dir="./tmp/mnist_convnet_model"
+    if os.path.exists(model_dir):
+        for f in os.listdir(model_dir):
+            fp = os.path.join(model_dir, f)
+            try:
+                if os.path.isfile(fp):
+                    os.remove(fp)
+                elif os.path.isdir(fp):
+                    for f2 in os.listdir(fp):
+                        fp2 = os.path.join(fp, f2)
+                        os.remove(fp2)
+#                    shutil.rmtree(fp)
+            except Exception as e:
+                print("ERROR", e)
+                pass
 
     # Create the Estimator
-    mnist_classifier = tf.estimator.Estimator(
-        model_fn=cnn_model_fn,
-        model_dir="./tmp/mnist_convnet_model")
-    print("\n\n \t \t \t estimator defined \n \n \n ")
+    model = tf.estimator.Estimator(model_fn=create_cnn_model, model_dir=model_dir)
 
-    # Specifiy Training
-    train_input_fn = tf.estimator.inputs.numpy_input_fn(
-        x={"x": x_train},
-        y=y_train,
-        batch_size=batch_size,
-        num_epochs=num_epochs,
-        shuffle=True)
+    # Train the model
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(x={"x": x_train},
+                                                        y=y_train,
+                                                        batch_size=batch_size,
+                                                        num_epochs=1,
+                                                        shuffle=True)
+    learning_curve = []
+    for e in range(num_epochs):
+        print("\nStart training for epoch {0} ...".format(e+1))
+        model.train(input_fn=train_input_fn)
+        print("\nStart validating for epoch {0} ...".format(e+1))
+        val_result = validate(x_valid, y_valid, model)
+        print("\nValidation result for epoch {0}: {1}".format(e+1, val_result))
+        learning_curve.append(val_result["accuracy"])
 
-    print("\n\n \t \t \t train_input_fn created \n \n \n ")
+    return learning_curve, model
 
-    mnist_classifier.train(
-        input_fn=train_input_fn,
-        steps=20000,
-        hooks=[logging_hook])
 
-    print("\n\n \t \t \t mnist_classifier.train called \n \n \n ")
+def validate(x_valid, y_valid, model):
+    # Validates the network.
+    
+    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": x_valid},
+        y=y_valid,
+        shuffle=False)
+    eval_results = model.evaluate(input_fn=eval_input_fn)
 
-    # TODO Evaluate the model and print results
-    # eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-    #     x={"x": eval_data},
-    #     y=eval_labels,
-    #     num_epochs=1,
-    #     shuffle=False)
-    #     eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
-    # print(eval_results)
-
-    # model = mnist_classifier
-
-    # return learning_curve, model  # TODO: Return the validation error after each epoch (i.e learning curve) and your model
-
+    return eval_results
 
 def test(x_test, y_test, model):
-    # TODO: test your network here by evaluating it on the test data
-    return test_error
+    # Test your network here by evaluating it on the test data.
+
+    eval_results = validate(x_test, y_test, model)
+    return 1 - eval_results["accuracy"]
 
 
 if __name__ == "__main__":
@@ -189,7 +205,8 @@ if __name__ == "__main__":
     parser.add_argument("--input_path", default="./", type=str, nargs="?",
                         help="Path where the data is located. If the data is not available it will be downloaded first")
     parser.add_argument("--learning_rate", default=1e-3, type=float, nargs="?", help="Learning rate for SGD")
-    parser.add_argument("--num_filters", default=16, type=int, nargs="?", # default changed from 32 to 16
+    # Changed default value for num_filters from 32 to 16.
+    parser.add_argument("--num_filters", default=16, type=int, nargs="?",
                         help="The number of filters for each convolution layer")
     parser.add_argument("--batch_size", default=128, type=int, nargs="?", help="Batch size for SGD")
     parser.add_argument("--epochs", default=12, type=int, nargs="?",
@@ -214,14 +231,18 @@ if __name__ == "__main__":
 
     test_error = test(x_test, y_test, model)
 
+    # print(test_error)
+
     # save results in a dictionary and write them into a .json file
     results = dict()
     results["lr"] = lr
     results["num_filters"] = num_filters
     results["batch_size"] = batch_size
     results["filter_size"] = filter_size
-    results["learning_curve"] = learning_curve
+    results["learning_curve"] = ["{}".format(i) for i in learning_curve]
     results["test_error"] = test_error
+
+    # print("learning_curve", learning_curve)
 
     path = os.path.join(args.output_path, "results")
     os.makedirs(path, exist_ok=True)
--- exercise2/random_search.py
+++ exercise2/random_search.py
@@ -7,10 +7,17 @@ import hpbandster.core.nameserver as hpns
 from hpbandster.optimizers import RandomSearch
 
 import ConfigSpace as CS
+import ConfigSpace.hyperparameters as CSH
+
 from hpbandster.core.worker import Worker
 import argparse
 
-from cnn_mnist_solution import mnist
+import cnn_mnist
+from cnn_mnist import mnist#, train_and_validate, test
+
+import numpy as np
+import json
+import os
 
 
 class MyWorker(Worker):
@@ -35,12 +42,23 @@ class MyWorker(Worker):
         num_filters = config["num_filters"]
         batch_size = config["batch_size"]
         filter_size = config["filter_size"]
-
         epochs = budget
 
-        # TODO: train and validate your convolutional neural networks here
+        print("CONFIG: ", config)
+        print("BUDGET: ", budget)
+        print("KWARGS: ", kwargs)
 
-        # TODO: We minimize so make sure you return the validation error here
+        # Train and validate your convolutional neural networks here
+        cnn_mnist.num_filters = num_filters
+        cnn_mnist.filter_size = filter_size
+        cnn_mnist.lr = lr
+        learning_curve, model = cnn_mnist.train_and_validate(self.x_train, self.y_train, self.x_valid, self.y_valid, epochs, lr, num_filters, batch_size, filter_size)
+
+        print("learning_curve: ", learning_curve)
+
+        validation_error = 1-float(learning_curve[-1])
+        
+
         return ({
             'loss': validation_error,  # this is the a mandatory field to run hyperband
             'info': {}  # can be used for any user-defined information - also mandatory
@@ -49,16 +67,25 @@ class MyWorker(Worker):
     @staticmethod
     def get_configspace():
         config_space = CS.ConfigurationSpace()
+        
+        learning_rate = CSH.UniformFloatHyperparameter('learning_rate', lower=1e-4, upper=1e-1, default_value=1e-3, log=True)
+        
+        batch_size = CSH.UniformIntegerHyperparameter('batch_size', lower=16, upper=128, default_value=128, log=True)
+        
+        num_filters = CSH.UniformIntegerHyperparameter('num_filters', lower=8, upper=64, default_value=16, log=True)
+        
+        filter_size = CSH.CategoricalHyperparameter('filter_size', [3, 5], default_value=3)
 
-        # TODO: Implement configuration space here. See https://github.com/automl/HpBandSter/blob/master/hpbandster/examples/example_5_keras_worker.py  for an example
+        config_space.add_hyperparameters([learning_rate, batch_size, num_filters, filter_size])
 
         return config_space
 
 
 parser = argparse.ArgumentParser(description='Example 1 - sequential and local execution.')
+# Changed default values for budget from 12 to 6 and for n_iterations from 20 to 50.
 parser.add_argument('--budget', type=float,
-                    help='Maximum budget used during the optimization, i.e the number of epochs.', default=12)
-parser.add_argument('--n_iterations', type=int, help='Number of iterations performed by the optimizer', default=20)
+                    help='Maximum budget used during the optimization, i.e the number of epochs.', default=6)
+parser.add_argument('--n_iterations', type=int, help='Number of iterations performed by the optimizer', default=50)
 args = parser.parse_args()
 
 # Step 1: Start a nameserver
@@ -105,12 +132,111 @@ print('Best found configuration:', id2config[incumbent]['config'])
 
 # Plots the performance of the best found validation error over time
 all_runs = res.get_all_runs()
+
+# Could cause problems on the pc pools
 # Let's plot the observed losses grouped by budget,
-import hpbandster.visualization as hpvis
+# import hpbandster.visualization as hpvis
+# hpvis.losses_over_time(all_runs)
+# import matplotlib.pyplot as plt
+# plt.savefig("hn_random_search.png")
 
-hpvis.losses_over_time(all_runs)
 
+
+# Retrain the best configuration (called incumbent) and compute the test error
+
+bestconfig = id2config[incumbent]['config']
+
+# hyperparameters
+lr = bestconfig['learning_rate']
+num_filters = bestconfig['num_filters']
+batch_size = bestconfig['batch_size']
+filter_size = bestconfig['filter_size']
+epochs = 12
+
+# train and test convolutional neural network
+learning_curve, model = cnn_mnist.train_and_validate(w.x_train, w.y_train, w.x_valid, w.y_valid, epochs, lr, num_filters, batch_size, filter_size)
+
+test_error = cnn_mnist.test(w.x_test, w.y_test, model)
+
+# save results in a dictionary and write them into a .json file
+results = dict()
+results["lr"] = lr
+results["num_filters"] = num_filters
+results["batch_size"] = batch_size
+results["filter_size"] = filter_size
+results["learning_curve"] = ["{}".format(i) for i in learning_curve]
+results["test_error"] = test_error
+
+# print("learning_curve", learning_curve)
+
+path = os.path.join("./", "results")
+os.makedirs(path, exist_ok=True)
+
+fname = os.path.join(path, "hn_results_run_%d.json" % 9999)
+
+fh = open(fname, "w")
+json.dump(results, fh)
+fh.close()
+
+
+
+# Fix for plotting problems on pc pools
+# print("all_runs:", all_runs)
+
+all_runs_serialized = {}
+for i, v in enumerate(all_runs):
+    # print(v)
+    # print(type(v))    
+    
+    all_runs_serialized[i] = {}
+    all_runs_serialized[i]['config_id'] = v.config_id
+    all_runs_serialized[i]['budget'] = v.budget
+    all_runs_serialized[i]['loss'] = v.loss
+    all_runs_serialized[i]['time_stamps'] = "{submitted} (submitted), {started} (started), {finished} (finished)\n".format(**v.time_stamps)
+    all_runs_serialized[i]['info'] = v.info
+
+fname = os.path.join(path, "hn_allruns_results_run_%d.json" % 9999)
+
+fh = open(fname, "w")
+json.dump(all_runs_serialized, fh)
+fh.close()
+
+# Compare source of:
+#     import hpbandster.visualization as hpvis
+#     hpvis.losses_over_time(all_runs)
 import matplotlib.pyplot as plt
-plt.savefig("random_search.png")
+get_loss_from_run_fn = lambda r: r.loss
+budgets = set([r.budget for r in all_runs])
+data = {}
+for b in budgets:
+    data[b] = []
+for r in all_runs:
+    if r.loss is None:
+        continue
+    b = r.budget
+    t = r.time_stamps['finished']
+    l = get_loss_from_run_fn(r)
+    data[b].append((t,l))
 
-# TODO: retrain the best configuration (called incumbent) and compute the test error
+for b in budgets:
+    data[b].sort()
+
+
+fig, ax = plt.subplots()
+
+for i, b in enumerate(budgets):
+    data[b] = np.array(data[b])
+    #print(data[b])
+    #print(data[b][:,0])
+    #print(data[b][:,1])
+    #print(range(len(data[b][:,0])))
+    ax.scatter(range(1, len(data[b][:,0])+1), data[b][:,1], label='data')
+    
+    ax.step(range(1, len(data[b][:,0])+1), np.minimum.accumulate(data[b][:,1]), where='post')
+
+ax.set_title('Validation errors over the different iterations')
+ax.set_xlabel('iteration')
+ax.set_ylabel('validation error')
+plt.xlim(0, len(data[b][:,0])+1)
+ax.legend()
+plt.savefig("hn_random_search.png")
