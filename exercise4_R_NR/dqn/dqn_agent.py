223ee5e8abe7be7722f1c9c61b3462089c059055
ah663 <amadeus.hovekamp@rwth-aachen.de>
2018-12-29 20:59:18 +0100

    CartPole initiated and dummy 'learner' implemented

--- exercise4_R_NR/dqn/dqn_agent.py
+++ exercise4_R_NR/dqn/dqn_agent.py
@@ -4,7 +4,8 @@ from dqn.replay_buffer import ReplayBuffer
 
 class DQNAgent:
 
-    def __init__(self, Q, Q_target, num_actions, discount_factor=0.99, batch_size=64, epsilon=0.05):
+    def __init__(self, Q, Q_target, num_actions, discount_factor=0.99,
+                 batch_size=64, epsilon=0.05):
         """
          Q-Learning agent for off-policy TD control using Function Approximation.
          Finds the optimal greedy policy while following an epsilon-greedy policy.
@@ -17,9 +18,9 @@ class DQNAgent:
             batch_size: Number of samples per batch.
             epsilon: Chance to sample a random action. Float betwen 0 and 1.
         """
-        self.Q = Q      
+        self.Q = Q
         self.Q_target = Q_target
-        
+
         self.epsilon = epsilon
 
         self.num_actions = num_actions
@@ -43,18 +44,18 @@ class DQNAgent:
 
         # TODO:
         # 1. add current transition to replay buffer
-        # 2. sample next batch and perform batch update: 
-        #       2.1 compute td targets: 
+        # 2. sample next batch and perform batch update:
+        #       2.1 compute td targets:
         #              td_target =  reward + discount * max_a Q_target(next_state_batch, a)
         #       2.2 update the Q network
         #              self.Q.update(...)
         #       2.3 call soft update for target network
         #              self.Q_target.update(...)
-   
+
 
     def act(self, state, deterministic):
         """
-        This method creates an epsilon-greedy policy based on the Q-function approximator and epsilon (probability to select a random action)    
+        This method creates an epsilon-greedy policy based on the Q-function approximator and epsilon (probability to select a random action)
         Args:
             state: current state input
             deterministic:  if True, the agent should execute the argmax action (False in training, True in evaluation)
@@ -64,15 +65,22 @@ class DQNAgent:
         r = np.random.uniform()
         if deterministic or r > self.epsilon:
             # TODO: take greedy action (argmax)
-            # action_id = ...
+            # print("state\n", state)
+            state = state.astype('float32').reshape(1, 4)
+
+            prediction = self.Q.predict(self.sess, state)
+            print("prediction:\n", prediction)
+            action_id = np.argmax(prediction)
+
         else:
 
             # TODO: sample random action
-            # Hint for the exploration in CarRacing: sampling the action from a uniform distribution will probably not work. 
+            # Hint for the exploration in CarRacing: sampling the action from a uniform distribution will probably not work.
             # You can sample the agents actions with different probabilities (need to sum up to 1) so that the agent will prefer to accelerate or going straight.
             # To see how the agent explores, turn the rendering in the training on and look what the agent is doing.
-            # action_id = ...
-          
+
+            action_id = np.random.randint(0, self.num_actions)
+
         return action_id
 
 
